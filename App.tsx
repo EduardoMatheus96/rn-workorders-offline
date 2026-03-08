8810d1568ffe71765a43742795ea4e77bb6e730d
Eduardo Matheus de Menezes Souto <eduardomatheusuesc@gmail.com>
2026-03-08 01:08:33 -0300

    feat: configure RealmProvider with WorkOrder and SyncMeta schemas

--- App.tsx
+++ App.tsx
@@ -1,47 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
 import './global.css';
+
+import { RealmProvider } from '@realm/react'; 
+import { StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { SyncMeta } from './src/database/schemas/SyncMetaSchema';
+import { WorkOrder } from './src/database/schemas/WorkOrderSchema';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
+    <RealmProvider
+    schema={[WorkOrder, SyncMeta]}
+    schemaVersion={1}
+  >
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
     </SafeAreaProvider>
+  </RealmProvider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
