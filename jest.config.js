cc2856b67e6af3ceac47c8b0280553c787e0b23f
Eduardo Matheus de Menezes Souto <eduardomatheusuesc@gmail.com>
2026-03-08 20:47:18 -0300

    chore: add react-native-vector-icons to Jest transform ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,10 @@ module.exports = {
   preset: 'react-native',
   moduleNameMapper: {
     '\\.css$': '<rootDir>/__mocks__/fileMock.js',
+    '@react-native-community/netinfo': '<rootDir>/node_modules/@react-native-community/netinfo/jest/netinfo-mock.js',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|realm|@realm/react|@realm/fetch|@react-navigation|react-native-css-interop)/)',
+    'node_modules/(?!(react-native|@react-native|realm|@realm/react|@realm/fetch|@react-navigation|react-native-css-interop|react-native-vector-icons)/)',
   ],
   setupFiles: [
     './node_modules/react-native-gesture-handler/jestSetup.js',
