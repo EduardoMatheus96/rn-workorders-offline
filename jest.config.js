3cb6325d0b491385ace9dbd6f768470f4fa6bf87
Eduardo Matheus de Menezes Souto <eduardomatheusuesc@gmail.com>
2026-03-08 15:40:28 -0300

    fix: add gesture handler jest setup to resolve native module error

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
     '\\.css$': '<rootDir>/__mocks__/fileMock.js',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|realm|@realm/react|@realm/fetch)/)',
+    'node_modules/(?!(react-native|@react-native|realm|@realm/react|@realm/fetch|@react-navigation|react-native-css-interop)/)',
+  ],
+  setupFiles: [
+    './node_modules/react-native-gesture-handler/jestSetup.js',
   ],
 };
