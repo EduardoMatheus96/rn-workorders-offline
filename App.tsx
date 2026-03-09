ceef2c3f1e1f0b4382f64a0248c18ca5eb12ef8b
Eduardo Matheus de Menezes Souto <eduardomatheusuesc@gmail.com>
2026-03-08 23:37:17 -0300

    fix: wrap app with GestureHandlerRootView

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import './global.css';
 
 import { RealmProvider } from '@realm/react'; 
-import { StatusBar, useColorScheme } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { SyncMeta } from './src/database/schemas/SyncMetaSchema';
 import { WorkOrder } from './src/database/schemas/WorkOrderSchema';
@@ -13,18 +14,21 @@ function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <RealmProvider
-    schema={[WorkOrder, SyncMeta]}
-    schemaVersion={1}
-  >
-    <SafeAreaProvider>
-        <NavigationContainer>
+    <GestureHandlerRootView style={styles.root}>
+      <RealmProvider schema={[WorkOrder, SyncMeta]} schemaVersion={1}>
+        <SafeAreaProvider>
+          <NavigationContainer>
             <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
             <RootNavigator />
-        </NavigationContainer>
-    </SafeAreaProvider>
-  </RealmProvider>
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </RealmProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  root: { flex: 1 },
+});
 
 export default App;
